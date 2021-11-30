c6b607ca98206d283ccac52d0063c3c5c8508a49
philipjonlewis <philipjonlewis@gmail.com>
2021-11-30 15:05:54 +0800

    fixed form validation with js and css

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,21 @@
-console.log("this is working");
+const label = document.querySelectorAll(".form-element");
+const button = document.querySelector(".form-button-container button");
 
+label.forEach((x) => {
+  x.children[1].addEventListener("input", (e) => {
+    if (Array.from(label).every((x) => x.children[1].validity.valid)) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  });
+});
+
+// console.log(label);
+
+// console.dir(button.disabled);
+// document.addEventListener("invalid", () => {
+//   if (fromInputValidity) {
+//     button.disabled = false;
+//   }
+// });
